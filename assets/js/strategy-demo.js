a41817b98151c59227d2b1c205d6ea450e3b01a1
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-02-04 20:10:32 -0800

    working featured module strategy

--- assets/js/strategy-demo.js
+++ assets/js/strategy-demo.js
@@ -10,7 +10,19 @@ $( document ).ready( function( )
 		{
 			return '<div class="module ' + this.type + '">Module: ' + this.type + '</div>';
 		};
+		
+		this.tag = function( tagName )
+		{
+			return _( this.tags ).detect( function( tag ){ return tag[ tagName ]; } );
+		};
 	};
+	
+	//--------------------------------------------------------
+	// MODULE COLLECTION
+	//--------------------------------------------------------
+	
+	// make a collection of strategies
+	var strategies = [ ];
 	
 	// make a collection of modules
 	var modules = [ ];
@@ -29,13 +41,51 @@ $( document ).ready( function( )
 	// push ten other modules to the array
 	// these will be of other random colors to set them apart
 	// they have no tags and will not be affected by placement strategies
-	_( _.range( 10 ) ).each( function( )
+	_( 10 ).times( function( )
 	{
 		modules.push( new Module( 'green', [ ] ) );
 	} );
 	
+	//--------------------------------------------------------
+	// STRATEGIES
+	//--------------------------------------------------------
+	
+	// strategy to put the most recent module tagged 'featured' first in the list
+	var featuredModuleStrategy = function( modules )
+	{
+		// select all modules with a featured tag
+		var featured = _( modules ).select( function( module ){ return module.tag( 'featured' ); } );
+		
+		// find the highest values featured item
+		var feature = _( featured ).chain( ).sortBy( function( module )
+		{
+			return module.tag( 'featured' ).featured;
+		}
+		).first( ).value( );
+
+		// remove the featured module from the modules list
+		modules.splice( $.inArray( feature, modules ), 1 );
+		
+		// add the features module to the front of the list
+		modules.unshift( feature );
+		
+		return modules;
+	};
+	
+	strategies.push( featuredModuleStrategy );
+	
+	//--------------------------------------------------------
+	// MODULE DISPLAY
+	//--------------------------------------------------------
+	
+	// randomize the modules, just for kicks
+	modules = _( modules ).sortBy( Math.random );
+	
+	// compose the display strategies
+	var composition = _.compose.apply( this, strategies );
+	
 	// display the modules
-	_( modules ).each( function( module )
+	_( composition( modules ) ).each( function( module )
 	{
 		$( '#modules' ).append( module.toElement( ) );
 	} );
